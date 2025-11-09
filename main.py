01f4d96086ae7e231f120e133f9a3f44dcd39146
rplague <wcx0717@outlook.com>
2025-11-09 19:32:42 +0800

    feat: 设计标准词汇表第三次操作
    - 设计标准词汇表构建程序，实现向量高速运算。
    
    	修改：     main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 
 import config_operator
 import basic_program
-from mariadb_operator import Db_operator
+import mariadb_operator
+import qdrant_operator
 from ai_modules import text_vectorization
 import xml_operator
 
@@ -16,6 +17,13 @@ if not situation:
 situation = basic_program.init_program()
 if not situation:
 	sys.exit(1)
+
+'''
+脚本任务：
+- 将标准词汇表中所有条目的唯一向量信息导入qdrant同名集合中
+
+'''
+
 basic_program.log_message("正在获取 config 信息")
 config_data = config_operator.get_config_data()
 start_index = config_data["start_index"]
@@ -23,7 +31,7 @@ basic_program.log_message("成功获取 config 信息")
 
 basic_program.log_message("正在获取 标准词汇表-中文 信息")
 try:
-	mariadb = Db_operator()
+	mariadb = mariadb_operator.Db_operator()
 	result = mariadb.safe_db_operation(
 		"SELECT id, 词语, XML含义 FROM chn_wordlist WHERE id > ?", 
 		params=(start_index,), 
@@ -34,23 +42,33 @@ except Exception as e:
 	sys.exit(1)
 basic_program.log_message("成功获取 标准词汇表-中文 信息")
 
-def process_main(id_word_xml_data_tup):
-	"""任务：
-		- 从标准的xml读取来自初融的释义
-		- 结合释义生成新的向量坐标并统合信息生成新的xml
-		- 存储到数据库"""
-	id_num, word, xml = id_word_xml_data_tup
-
+basic_program.log_message(f"正在检测 {target_collection} 集合")
+target_collection = "chn_wordlist"
+qdrant = qdrant_operator.Db_operator()
+if target_collection in qdrant.safe_qdrant_operation("list_collections"):
+	basic_program.log_message(f"正在创建 {target_collection} 集合")
 	try:
-		data_text = xml_operator.test_operation_002_1_1(xml)
-		vectorization_text = text_vectorization(data_text)
-		new_xml = xml_operator.test_operation_002_2(xml, "BGE_large_zh_configT01", vectorization_text)
+		qdrant.safe_qdrant_operation("create_collection", target_collection, )
+	except Exception as e:
+		basic_program.log_message(f"无法创建 {target_collection} 集合\n    {e}", 50)
+		sys.exit(1)
+	basic_program.log_message(f"成功创建 {target_collection} 集合")
+basic_program.log_message(f"{target_collection} 集合已准备就绪")
 
-		mariadb = Db_operator()
-		result = mariadb.safe_db_operation(
-				"UPDATE chn_wordlist SET XML含义 = ? WHERE id = ?", 
-				params=(new_xml, id_num)
-			)
+def process_main(id_word_xml_data_tup):
+	"""
+	任务：
+	- 从标准的xml读取向量数据
+	- 组合信息创建数据点并导入数据库
+	"""
+	id_num, word, xml = id_word_xml_data_tup
+	target_collection = "chn_wordlist"
+	try:
+		vector_partial = xml_operator.xml_vector_partial_retrieval(xml, "BGE_large_zh_configT01").tolist()
+		
+		qdrant = qdrant_operator.Db_operator()
+		qdrant.create_point_struct(id_num, vector_partial)
+		qdrant.safe_qdrant_operation("upsert_points", target_collection, [qdrant.create_point_struct(id_num, vector_partial)])
 		basic_program.log_message(f"id 为 {id_num} 的条目已完成既定操作", printing = False)
 		return True
 	except Exception as e:
