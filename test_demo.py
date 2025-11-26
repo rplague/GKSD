b30f3c16d0ef2ee061a85c2b7ca1f5d1fbaadb4e
rplague <wcx0717@outlook.com>
2025-11-26 11:54:38 +0800

    feat: 添加自动化逻辑向量精准度评分程序
    	修改：     test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,14 +1,12 @@
 import sys
+from tqdm import tqdm
 import traceback
+import numpy as np
 
+import config_operator
 import basic_program
 import gksd_operator
-'''
-脚本任务：
-- 测试新的log程序
-
-'''
-
+import logicfile_operator
 
 # 初始化
 situation = basic_program.boot()
@@ -20,11 +18,67 @@ if not situation:
 
 GKSD_operator = gksd_operator.GKSD_operator()
 
-# answer_list = GKSD_operator.safe_db_operation("search", name = "人工智能")
+logicfile_set = logicfile_operator.LogicfileSetIndex("data/PartOf.json")
+now_position = None
 
-# print("搜索结果")
-# for answer in answer_list:
-# 	print(answer[0], "\t", answer[1], "\n")
-
-
-GKSD_operator.safe_db_operation("upsert", name = "地铁")
+logicfile = logicfile_operator.LogicfileIndex("data/PartOf_data_statistics_summary.json")
+delta_v = []
+for div in range(len(logicfile)):
+	delta_v.append(logicfile[div]["mean"])
+print("待测试向量差装载完成")
+succes_num = 0
+for data_set_index in tqdm(range(len(logicfile_set)), desc="处理数据集"):
+	data_set = logicfile_set[data_set_index]
+	target_id = data_set["word_id"]
+	answer_id = data_set["master_id"]
+	target = GKSD_operator.safe_db_operation("search", id_num=target_id, log_printing=False)[0]
+	vector_np = np.array(target["vector"])
+	delta_np = np.array(delta_v)
+	answer_vector = (vector_np + delta_np).tolist()
+	answer_list = GKSD_operator.safe_db_operation("search", vector=answer_vector, log_printing=False, limit=10)
+	if answer_id in [answer["id"] for answer in answer_list]:
+		succes_num += 1
+point = (succes_num / len(logicfile_set)) * 100
+print("运算完毕 结果10\n成功率 ", point, "%")
+succes_num = 0
+for data_set_index in tqdm(range(len(logicfile_set)), desc="处理数据集"):
+	data_set = logicfile_set[data_set_index]
+	target_id = data_set["word_id"]
+	answer_id = data_set["master_id"]
+	target = GKSD_operator.safe_db_operation("search", id_num=target_id, log_printing=False)[0]
+	vector_np = np.array(target["vector"])
+	delta_np = np.array(delta_v)
+	answer_vector = (vector_np + delta_np).tolist()
+	answer_list = GKSD_operator.safe_db_operation("search", vector=answer_vector, log_printing=False, limit=20)
+	if answer_id in [answer["id"] for answer in answer_list]:
+		succes_num += 1
+point = (succes_num / len(logicfile_set)) * 100
+print("运算完毕 结果20\n成功率 ", point, "%")
+succes_num = 0
+for data_set_index in tqdm(range(len(logicfile_set)), desc="处理数据集"):
+	data_set = logicfile_set[data_set_index]
+	target_id = data_set["word_id"]
+	answer_id = data_set["master_id"]
+	target = GKSD_operator.safe_db_operation("search", id_num=target_id, log_printing=False)[0]
+	vector_np = np.array(target["vector"])
+	delta_np = np.array(delta_v)
+	answer_vector = (vector_np + delta_np).tolist()
+	answer_list = GKSD_operator.safe_db_operation("search", vector=answer_vector, log_printing=False, limit=40)
+	if answer_id in [answer["id"] for answer in answer_list]:
+		succes_num += 1
+point = (succes_num / len(logicfile_set)) * 100
+print("运算完毕 结果40\n成功率 ", point, "%")
+succes_num = 0
+for data_set_index in tqdm(range(len(logicfile_set)), desc="处理数据集"):
+	data_set = logicfile_set[data_set_index]
+	target_id = data_set["word_id"]
+	answer_id = data_set["master_id"]
+	target = GKSD_operator.safe_db_operation("search", id_num=target_id, log_printing=False)[0]
+	vector_np = np.array(target["vector"])
+	delta_np = np.array(delta_v)
+	answer_vector = (vector_np + delta_np).tolist()
+	answer_list = GKSD_operator.safe_db_operation("search", vector=answer_vector, log_printing=False, limit=80)
+	if answer_id in [answer["id"] for answer in answer_list]:
+		succes_num += 1
+point = (succes_num / len(logicfile_set)) * 100
+print("运算完毕 结果80\n成功率 ", point, "%")
