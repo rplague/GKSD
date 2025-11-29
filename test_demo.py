d9e3aa7df6625c5071b6c3ac5a9714d817276305
rplague <wcx0717@outlook.com>
2025-11-29 20:43:22 +0800

    feat: 添加了将数据集加入数据库的脚本
    	修改：     test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -4,31 +4,29 @@ from tqdm import tqdm
 import config_operator
 import basic_program
 import gksd_operator
+import logicfile_operator
 import xml_operator
 
-# 初始化
-situation = basic_program.boot()
-if not situation:
-	sys.exit(1)
-situation = basic_program.init_program()
-if not situation:
-	sys.exit(1)
+basic_program.log_message("开始脚本任务 task04", 0)
 
 GKSD_operator = gksd_operator.GKSD_operator()
 
-for i in tqdm(range(5, 6), desc="处理进度", unit="条"):
-	answer_list = GKSD_operator.mariadb_operator.safe_db_operation(
-		"SELECT XML含义 FROM chn_wordlist WHERE id = ?", 
-		params=(i,),
+logicfile_set = logicfile_operator.LogicfileSetIndex("data/PartOf.json")
+for index in range(len(logicfile_set)):
+	item = logicfile_set[index]
+	xml_data = GKSD_operator.mariadb_operator.safe_db_operation(
+		"SELECT XML含义 FROM chn_wordlist WHERE id = ?",
+		params=(item["word_id"],),
 		fetch=True
+	)[0][0]
+	xml_data = xml_operator.xml_unsure_relational_partial_adding(xml_data, "Initial_Thaw_DS", "PartOf", item["master_id"], confidence=0.2)
+	if index == 0:
+		print(xml_data, "\n继续则输入[qweasd]")
+		if input(">>> ") != "qweasd":
+			break
+	xml_data = GKSD_operator.mariadb_operator.safe_db_operation(
+		"UPDATE chn_wordlist SET XML含义 = ? WHERE id = ?;",
+		params=(xml_data, item["word_id"],),
+		fetch=False
 	)
-	xml_data = answer_list[0][0]
-	new_xml_data = xml_operator.xml_rebuild(xml_data)
-	print(new_xml_data)
-	print(xml_data)
-	# GKSD_operator.mariadb_operator.safe_db_operation(
-	# 	"UPDATE chn_wordlist SET XML含义 = ? WHERE id = ?", 
-	# 	params=(new_xml_data,i,),
-	# 	fetch=False
-	# )
-	
+	print("\r", index, "/", len(logicfile_set), end="")
