8f2bee7e94d94fd070935a567ab1f046212206e1
rplague <wcx0717@outlook.com>
2025-12-15 18:44:22 +0800

    feat: 添加基于高级操作的task07脚本
    - 添加不确定partof数量
    - 为之后逻辑关系判断做准备
    
    	修改：     test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,39 +1,20 @@
 import sys
-from tqdm import tqdm
 
-import config_operator
 import basic_program
 import gksd_operator
 import xml_operator
+import ai_modules
 
-basic_program.log_message("开始脚本任务 task06", 0)
+basic_program.log_message("开始脚本任务 task07", 0)
 
-# 将重复自引用的错误partof删除
+# 通过新的算法从新添加partof，增加待确认partof数量为之后逻辑推导建立基础
 
 GKSD_operator = gksd_operator.GKSD_operator()
 n = 25073
 target = 0
-for index in range(1,n):
-		xml_data_list = GKSD_operator.mariadb_operator.safe_db_operation(
-				"SELECT XML含义 FROM chn_wordlist WHERE id = ?",
-				params=(index,),
-				fetch=True
-		)
-		if xml_data_list: xml_data = xml_data_list[0][0]
-		else: raise
-		xml_data_check = xml_operator.xml_check(xml_data, auto=True, id_num=index)
-		if xml_data_check:
-			if type(xml_data_check) == str and target <= 10:
-					print(index, xml_data[:200], "\n", "---" * 20, "\n", xml_data_check[-400:], xml_data[-400:])
-					print("\n继续则输入[qweasd]")
-					if input(">>> ") != "qweasd":
-							break
-			if type(xml_data_check) == str:
-				xml_data = xml_data_check
-				target += 1
-			GKSD_operator.mariadb_operator.safe_db_operation(
-					"UPDATE chn_wordlist SET XML含义 = ? WHERE id = ?;",
-					params=(xml_data, index,),
-					fetch=False
-			)
-		print("\r", index, "/", str(n), end="")
+for target_index in range(1,n):
+	word_data_list = GKSD_operator.safe_db_operation("search", id_num=target_index)
+	if word_data_list: word_data = word_data_list[0]
+	else: raise
+	GKSD_operator.advance_db_operation("auto_search_partof", word_data=word_data)
+	print("\r", target_index, "/", str(n), end="")
